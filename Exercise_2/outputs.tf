5fbf9519372b982646509a666300d0de16a75478
Dean Bui <linh.cn3@gmail.com>
2024-05-18 15:51:45 +0700

    update : submit

--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,4 @@
 # TODO: Define the output variable for the lambda function.
+output "lambda_function_invoke_arn" {
+  value = aws_lambda_function.greet_lambda.invoke_arn
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,14 @@
-# TODO: Define the variable for aws_region
+variable "aws_region" {
+  type     = string
+  default = "us-east-1"
+}
+variable "aws_access_key_id" {
+  type     = string
+}
+variable "aws_secret_access_key" {
+  type     = string
+}
+variable "lambda_function_name" {
+  type     = string
+  default = "greet"
+}
